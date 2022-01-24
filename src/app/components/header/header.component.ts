e8378dab900dfa0c68fef026ff357ad5a5ee817f
Sijan <sijuichi1@gmail.com>
2022-01-24 00:31:01 -0800

    Final

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import {Router} from '@angular/router'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  title = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService, private router:Router) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
+
+  ngOnInit(): void {
+    // life cycle method, use most of the time when initializing code
+    // load when component loads put in here
+
+  }
+
+  toggleAddTask(){
+
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url == route;
+  }
+
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service'
+import {Task} from '../../Task'
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  message = ' Never forget your Tasks '
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks = tasks);
+  }
+
+  deleteTask(task: Task){
+    this.taskService.deleteTasks(task).subscribe(
+      () => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
+
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task)=>this.tasks.push(task));
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+}
